aa1aed40d770ddcd9ec2c3daecc78efafd8d514b
Azalia <Azalia@DESKTOP-S258JJS>
2024-10-13 23:29:14 +0300

    changed email adresses visibility

--- EmailAppTest/Program.cs
+++ EmailAppTest/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your email: ");
+        string emailAdressSender = Environment.GetEnvironmentVariable("emailAdressSender");
+        string emailAddressRecipient = Environment.GetEnvironmentVariable("emailAddressRecipient");
+        string password = Environment.GetEnvironmentVariable("password");
+        string body = "HELLO!";
+        string subject = "";
+        var email = new Email("Tester", emailAdressSender, emailAddressRecipient, subject, body);
+
+        email.AddAttachment(@"C:\Users\Azalia\Desktop\goose.jpeg");
+
+        email.BuildMessage();
+
+        var emailSender = new EmailSender("smtp.gmail.com", 465, true);
+        emailSender.SendEmail(email, emailAdressSender, password);
+    }
+}
+
+
+
