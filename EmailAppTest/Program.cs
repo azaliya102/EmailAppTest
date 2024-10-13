388c7e2de8472c45abda4664fb879c2acdbdb8b9
Azalia <Azalia@DESKTOP-S258JJS>
2024-10-13 23:29:14 +0300

    commited all classes

--- EmailAppTest/Program.cs
+++ EmailAppTest/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string emailAdress = "azaliamuratova628@gmail.com";
+        string password = Environment.GetEnvironmentVariable("password");
+        string body = "HELLO!";
+        string subject = "";
+        var email = new Email("Tester", emailAdress, "indonesianwesleysnipes@gmail.com", subject, body);
+
+        email.AddAttachment(@"C:\Users\Azalia\Desktop\goose.jpeg");
+
+        email.BuildMessage();
+
+        var emailSender = new EmailSender("smtp.gmail.com", 465, true);
+        emailSender.SendEmail(email, emailAdress, password);
+    }
+}
+
+
+
