48cc42fcd837efe1e21f0a90c48af37f7b508f08
Azalia <Azalia@DESKTOP-S258JJS>
2024-10-21 04:58:22 +0300

    everything for linq, will add join/filter queries later

---
+++ EverythingAboutLinQ/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+[Table("emp")]
+public class Employee
+{
+    [Key]
+    public int empno { get; set; }
+    public string ename { get; set; }
+    public string job { get; set; }
+    public int? mgr { get; set; }
+    public DateTime hiredate { get; set; }
+    public decimal sal { get; set; }
+    public decimal? comm { get; set; }
+    public int deptno { get; set; }
+}
